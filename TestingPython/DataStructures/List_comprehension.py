de2308fe2be939b6405953d5979f2d65de7d0fa4
Kjartan S. Lerøy <kjartan.l@gmail.com>
2018-09-19 11:13:32 +0200

    Added examples using list comprehension

---
+++ TestingPython/DataStructures/List_comprehension.py
@@ -0,0 +1,16 @@
+
+# Simple way to declare lists:
+pow2 = [2 ** x for x in range(10)]
+
+# Output: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+print(pow2)
+
+pow2 = [2 ** x for x in range(10) if x > 5]
+print(pow2)
+
+
+odd_numbers = [x for x in range(20) if x % 2 == 1]
+print(odd_numbers)
+
+cross_combination = [first + second for first in ['Alpha','Beta', 'Gamma'] for second in ['one','two', 'three']]
+print(cross_combination)
