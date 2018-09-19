62ec85d8a86bccd907db0c617f3022ad8813fe7a
Kjartan S. Lerøy <kjartan.l@gmail.com>
2018-09-19 11:13:55 +0200

    More list-examples (splicing, etc)

--- TestingPython/DataStructures/Lists.py
+++ TestingPython/DataStructures/Lists.py
@@ -1,17 +1,12 @@
-
-
 string_list = ["we", "are", "the", "knights", "who", "say", "ni"]
-
-# Print whole list
-print(string_list)
-
-#Print element
 print("Element 3 was '%s'" %string_list[3])
 
-#remove element, and replace it with another:
+# Check existance of an element in a list:
+print("Is 'knights' in the list? %s" %('knights' in string_list))
 string_list.remove("say")
-string_list.insert(5, "code")
+print("Was 'say' removed? %s" %("say" not in string_list))
 
+string_list.insert(5, "code")
 print(string_list)
 
 # Change existing elements
@@ -21,13 +16,35 @@ print(string_list)
 
 last_item = string_list.pop() # pop last item
 item_3 = string_list.pop(2) # pop item at index 2 
+print("Last item: %s" %last_item)
+print("Item #3: %s" %item_3)
+print("After poping: %s" %string_list)
 
-print("Last item was %s" %last_item)
-print("Item #3 was %s" %item_3)
-
-print("After poping those, the list looks like this: %s" %string_list)
-
-# append
 string_list.append("is")
 string_list.append("SUNG!")
-print("Final list: %s" %string_list)
+print("List: %s" %string_list) # ['we', 'are', 'knights', 'for whom', 'code', 'is', 'SUNG!']
+
+## SPLICING AND REPLACING ITEMS
+string_list[3:5] = ["by", "whom", "code"] #['we', 'are', 'knights', 'by', 'whom', 'code', 'is', 'SUNG!'] 
+string_list[-3:] = ["awsome", "code", "is", "regularly", "composed"]
+# ['we', 'are', 'knights', 'by', 'whom', 'awsome', 'code', 'is', 'regularly', 'composed']
+print("List: %s" %string_list)
+
+# splicing from the start, until the N-last element:
+string_list[:-7] = ["These", "are", "the", "hackers"]
+print("List: %s" %string_list)
+
+# Appending (1 element) and extending (1 or more elements)
+string_list.append("with")
+string_list.extend(["clarity", "and", "ease"])
+
+print(string_list)
+
+# Concatenation and repeated output 
+# NOTE: All elements must be  lists (i.e. list[x:y], not list[x], which is a string)
+another_list = string_list[3:4] + string_list[10:11] + string_list[6:8] + string_list[-4:]
+
+# ['hackers', 'composed', 'awsome', 'code', 'with', 'clarity', 'and', 'ease']
+print(another_list)
+print(another_list[:1] * 3) # ['hackers', 'hackers', 'hackers']
+
