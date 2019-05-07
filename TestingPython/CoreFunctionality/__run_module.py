f061d80d328754466044a574b2bda43a9a24f6ed
Kjartan Sæle Lerøy <kjsl@dips.no>
2019-05-07 08:22:33 +0200

    started reworking the whole menu structure to use sub-menus for each directory

---
+++ TestingPython/CoreFunctionality/__run_module.py
@@ -0,0 +1,14 @@
+
+def run_classes():
+    from CoreFunctionality import Classes
+
+def get_run_list():
+    return [run_classes]
+
+def get_display_list():
+    return ["  1. Classes", 
+            "  2. Command line args", 
+            "  3. Exceptions", 
+            "  4. File IO", 
+            "  5. Lambdas", 
+            "  6. Print formatting"]
