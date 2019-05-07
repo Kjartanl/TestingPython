57e8d53016aebb5160031a1b41a2dfa1d25cad26
Kjartan Sæle Lerøy <kjartan.l@gmail.com>
2019-05-07 08:22:33 +0200

    started reworking the whole menu structure to use sub-menus for each directory

--- TestingPython/menu_based_program.py
+++ TestingPython/menu_based_program.py
@@ -3,6 +3,44 @@ from Programs.ProgramWithMenu import functions as fn
 def run_for_loops():
     from Logic import For_loops
 
+def run_basic_logic():
+    from Logic import basic_logic
+
+def run_generators():
+    from Logic import Generators
+
+def run_dictionaries_code():
+    from DataStructures import Dictionaries
+
+def run_list_comprehensions():
+    from DataStructures import List_comprehension
+
+def run_lists():
+    from DataStructures import Lists
+    
+
+def display_sub_menu(disp_list, run_list):
+    print("SUB-MENU OPTIONS:")
+    print("-------------------")
+    for opt in disp_list:
+        print(opt)
+    inputValue = input("Item to run?")
+
+    try:
+        selected_option = int(inputValue)
+        print(selected_option)
+
+        selected_option -= 1
+            
+        if selected_option <= len(run_list):
+            run_list[selected_option]()
+    except ValueError:
+        print("Invalid option selected.")
+
+
+def run_core_functionality():
+    from CoreFunctionality import __run_module as corefun
+    display_sub_menu(corefun.get_display_list(), corefun.get_run_list())
 
 def main(): # Menu goes here
     list_options = [fn.accept_and_store, 
@@ -10,7 +48,13 @@ def main(): # Menu goes here
                     fn.compare_numbers,
                     fn.calculator, 
                     fn.remove_letter, 
-                    run_for_loops]
+                    run_for_loops, 
+                    run_basic_logic, 
+                    run_generators, 
+                    run_lists, 
+                    run_list_comprehensions, 
+                    run_dictionaries_code, 
+                    run_core_functionality]
 
 
     while(True):
@@ -25,9 +69,15 @@ def main(): # Menu goes here
         print("  4. Calculate")
         print("  5. Remove letter")        
         print("Simpler functions:")
-        print("  6. Run For Loops code")
+        print("  6. For Loops code")
+        print("  7. Basic Logic")
+        print("  8. Generators code")
+        print("  9. Lists")
+        print("  10. List comprehensions")
+        print("  11. Dictionaries")
         print("------")
-        print("7. (or 'Q') QUIT")
+        print("  12: Sub-menu")
+        print("'Q': QUIT")
         print("------")
 
         inputValue = input("Menu item?") 
@@ -44,9 +94,6 @@ def main(): # Menu goes here
             if selected_option > len(list_options):
                 continue
 
-            if(selected_option is 6):
-                break
-            
             list_options[selected_option]()
         except ValueError:
             print("Invalid option selected.")
