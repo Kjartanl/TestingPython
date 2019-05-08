27905a2e67b452d1e65f28d7a52329f22c6e3919
Kjartan Sæle Lerøy <kjartan.l@gmail.com>
2019-05-09 00:10:11 +0200

    super-minor thingy...

--- TestingPython/CoreFunctionality/numbers.py
+++ TestingPython/CoreFunctionality/numbers.py
@@ -2,9 +2,12 @@
 sum = 12 + 34
 print(f"sum was: {sum}")
 
+print(f'Note the difference when dividing with a non-integer as a result: {23/4}')
+
 remainder  = 23 % 5
 print(f"Remainder of 23 % 5 was: {remainder}")
 
 # NB: Notice the double // to get the whole number (here, '4'):
 dividend_or_whatever_it_is_called = 23 // 5
 print(f"The divident (is that the correct term?) was: {dividend_or_whatever_it_is_called}")
+
