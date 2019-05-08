ebaa7ae7a5fd8e720aa4d8d20893af8c742a23df
Kjartan Sæle Lerøy <kjartan.l@gmail.com>
2019-05-08 23:54:59 +0200

    added a couple more examples

---
+++ TestingPython/CoreFunctionality/numbers.py
@@ -0,0 +1,10 @@
+
+sum = 12 + 34
+print(f"sum was: {sum}")
+
+remainder  = 23 % 5
+print(f"Remainder of 23 % 5 was: {remainder}")
+
+# NB: Notice the double // to get the whole number (here, '4'):
+dividend_or_whatever_it_is_called = 23 // 5
+print(f"The divident (is that the correct term?) was: {dividend_or_whatever_it_is_called}")
--- TestingPython/CoreFunctionality/print_format.py
+++ TestingPython/CoreFunctionality/print_format.py
@@ -15,3 +15,10 @@ print("Number was %d" % inputStr)
 
 print("Numbers separated by tabs:")
 print("Nrs: \t| %d\t| %d\t| %d\t|" %(str2, str3, inputStr))
+
+print("We will now state something cool:")
+string_with_format = "This should contain a {} cool thing here: {}"
+input_cool_thing = input("(Quick, what do you want it to say?)")
+input_coolness_level = input("(..and how cool do you want it to be?)")
+
+print(string_with_format.format(input_coolness_level, input_cool_thing))
